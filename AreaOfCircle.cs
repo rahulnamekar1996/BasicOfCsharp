2469025a699b4cb20ec34bbe5c38a771ddae8bec
Owner <Owner@DESKTOP-2TPH2RF>
2024-05-22 00:50:23 +0530

    Add project files.

---
+++ AreaOfCircle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BasicOfCsharp
+{
+    internal class AreaOfCircle
+
+
+    {
+        static double calculateAreaOfCircle(double redius) 
+        {
+            return 3.14 * redius * redius;
+        
+        }
+        static void main(String[] args)
+            
+        {
+          Console.WriteLine( "Area of circle" +AreaOfCircle.calculateAreaOfCircle(100));
+
+        }
+    }
+}
